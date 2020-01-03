2a459617e63780691096e3de0e312ec975b0cba2
alxmrz <alexandr.moroz97@mail.ru>
2020-01-03 20:34:16 +0300

    added docker run

--- application/web/index.php
+++ application/web/index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use core\container\PHPDIContainer;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,4 +9,5 @@ $config = require __DIR__ . '/../config/config.php';
 $definitions = require __DIR__ . '/../config/definitions.php';
 $container = new PHPDIContainer($definitions);
 
-(new core\Application($container, $config))->run();
+(new core\Application($container, $config))->run();*/
+echo 'HEllo, World!';
